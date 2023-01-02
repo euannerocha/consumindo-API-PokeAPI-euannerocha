d3c1f9a005ae0ed415fa8fc1d7ce826c4a254246
euannerocha <annelisesr@gmail.com>
2023-01-02 02:46:40 -0300

    Requisições API para renderizar lista de todos os Pokemon, CSS e HTML e responsividade com mobile first

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,29 @@
-/* Monte sua lógica aqui */
+async function renderizaPokemons() {
+
+    const listaDePokemons = await consomePokeAPI()
+    console.log(listaDePokemons)
+}
+
+renderizaPokemons()
+
+
+async function renderizaPokemons() {
+
+    const ulTag = document.querySelector('#ul')
+
+    const listaDePokemons = await consomePokeAPI()
+
+    listaDePokemons.results.forEach(pokemon => {
+        
+        const numeroNaPokedex = pokemon.url.slice(34, -1)
+
+        ulTag.insertAdjacentHTML('beforeend', `
+            <li>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${numeroNaPokedex}.png" alt=${pokemon.name}>
+                <h3>${pokemon.name}</h3>
+            </li>
+        `)
+    })
+}
+
+renderizaPokemons()
--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -1,1 +1,18 @@
-/* Monte a lógica das requisições aqui */
+async function consomePokeAPI() {
+
+    const loading = document.querySelector('#loading')
+
+    const pokemonsDaAPI = await fetch('https://pokeapi.co/api/v2/pokemon')
+      .then(
+        response => response.json()
+      )
+      .catch(
+        error => console.log(error)
+      )
+
+    loading.style.display = 'none'
+
+    return pokemonsDaAPI
+}
+
+consomePokeAPI()
